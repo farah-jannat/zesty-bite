5d0b6fe78d4dec307b1974884df657ac06ee1fd1
“farah-jannat” <farahjannatmithila@gmail.com>
2025-06-10 17:13:13 +0600

    feat: take value from login input

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -4,14 +4,33 @@ import { LockKeyhole, Mail, Loader2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Link } from "react-router-dom";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-type Props = {};
+type LoginInputState = {
+  email: string;
+  password: string;
+};
 
-const Login = (props: Props) => {
+const Login = () => {
+  const [input, setInput] = useState<LoginInputState>({
+    email: "",
+    password: "",
+  });
+  const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
   const loading = false;
+  const loginSubmitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("input of login", input);
+  };
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <form className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4">
+      <form
+        onSubmit={loginSubmitHandler}
+        className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4"
+      >
         <div className="mb-4">
           <div className="relative">
             <h1 className="font-bold text-2xl">PlateEats</h1>
@@ -22,6 +41,9 @@ const Login = (props: Props) => {
             <Input
               type="email"
               placeholder="Email"
+              name="email"
+              value={input.email}
+              onChange={changeEventHandler}
               className="pl-10 focus-visible:ring-1"
             />
 
@@ -33,12 +55,15 @@ const Login = (props: Props) => {
             <Input
               type="password"
               placeholder="Password"
+              name="password"
+              value={input.password}
+              onChange={changeEventHandler}
               className="pl-10 focus-visible:ring-1"
             />
             <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
           </div>
         </div>
-        <div>
+        <div className="mb-10">
           {loading ? (
             <Button disabled className="w-full bg-orange hover:bg-hoverOrange">
               <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
@@ -52,10 +77,13 @@ const Login = (props: Props) => {
             </Button>
           )}
         </div>
-        <Separator/>
-        <p>Don't have an account? </p>
-        <Link to = "/signup">Signup</Link>
-       
+        <Separator />
+        <p className="mt-2">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-700">
+            Signup
+          </Link>
+        </p>
       </form>
     </div>
   );
