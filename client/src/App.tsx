b3b82dac1a916ebe38c306815428a14230262cd8
“farah-jannat” <farahjannatmithila@gmail.com>
2025-06-10 17:12:37 +0600

    feat: add app-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,32 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
-import { Button } from "./components/ui/button";
+import Login from "./auth/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./MainLayout";
+import Signup from "./auth/Signup";
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Button className="bg-orange">lets build app together</Button>
-      
-    </>
+    <main>
+      <RouterProvider router={appRouter}></RouterProvider>
+    </main>
   );
 }
 
