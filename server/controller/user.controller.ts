7ac62c347b2209e0146425954959ff80decbbe3a
“farah-jannat” <farahjannatmithila@gmail.com>
2025-06-13 22:23:02 +0600

    feat: update user-controllers

--- server/controller/user.controller.ts
+++ server/controller/user.controller.ts
@@ -2,6 +2,15 @@ import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
+import cloudinary from "../utils/cloudinary";
+import { generateToken } from "../utils/generateToken";
+import { generateVerificationCode } from "../utils/generateVerificationCode";
+import {
+  sendPasswordResetEmail,
+  sendResetSuccessEmail,
+  sendVerificationEmail,
+  sendWelcomeEmail,
+} from "../mailtrap/email";
 
 export const signup = async (req: Request, res: Response) => {
   try {
@@ -15,7 +24,7 @@ export const signup = async (req: Request, res: Response) => {
       });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const verificationToken = "ajkdhfkajhskjdf"; //varificationtoken will be here
+    const verificationToken = generateVerificationCode();
 
     user = await User.create({
       fullname,
@@ -25,6 +34,8 @@ export const signup = async (req: Request, res: Response) => {
       verificationToken,
       verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
     });
+    generateToken(res, user);
+    await sendVerificationEmail(email, verificationToken);
 
     const userWithoutPassword = await User.findOne({ email }).select(
       "-password"
@@ -57,7 +68,7 @@ export const login = async (req: Request, res: Response) => {
         message: "Incorrect email or password",
       });
     }
-
+    generateToken(res, user);
     user.lastLogin = new Date();
     await user.save();
 
@@ -97,6 +108,7 @@ export const verifyEmail = async (req: Request, res: Response) => {
     await user.save();
 
     // send welcome email
+    await sendWelcomeEmail(user.email, user.fullname);
 
     return res.status(200).json({
       success: true,
@@ -142,10 +154,10 @@ export const forgotPassword = async (req: Request, res: Response) => {
     await user.save();
 
     // send email
-    // await sendPasswordResetEmail(
-    //   user.email,
-    //   `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`
-    // );
+    await sendPasswordResetEmail(
+      user.email,
+      `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`
+    );
     return res.status(200).json({
       success: true,
       message: "Password reset link sent to your email",
@@ -177,7 +189,8 @@ export const resetPassword = async (req: Request, res: Response) => {
     user.resetPasswordTokenExpiresAt = undefined;
     await user.save();
 
-    // send success reset email here
+    // send success reset email
+    await sendResetSuccessEmail(user.email);
 
     return res.status(200).json({
       success: true,
@@ -202,6 +215,38 @@ export const checkAuth = async (req: Request, res: Response) => {
     return res.status(200).json({
       success: true,
       user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateProfile = async (req: Request, res: Response) => {
+  try {
+    const userId = req.id;
+    const { fullname, email, address, city, country, profilePicture } =
+      req.body;
+    // upload image on cloudinary
+    let cloudResponse: any;
+    cloudResponse = await cloudinary.uploader.upload(profilePicture);
+    const updatedData = {
+      fullname,
+      email,
+      address,
+      city,
+      country,
+      profilePicture,
+    };
+
+    const user = await User.findByIdAndUpdate(userId, updatedData, {
+      new: true,
+    }).select("-password");
+
+    return res.status(200).json({
+      success: true,
+      user,
+      message: "Profile updated successfully",
     });
   } catch (error) {
     console.error(error);
